ff499c1b65b5f0b6961c0be478736ea7500fcaca
kerolos <kerolos20140659@yahoo.com>
2025-05-07 00:49:05 +0200

    updating some files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.data_science_project import logger
 
 from src.data_science_project.pipeline.dataingestionpipeline import DataIngestionTrainingPipeline
 
-
+from src.data_science_project.pipeline.datavalidationpipeline import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -14,3 +14,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+# from src.data_science_project.pipeline.datavalidationpipeline import DataValidationTrainingPipeline
--- src/data_science_project/config/configuration.py
+++ src/data_science_project/config/configuration.py
@@ -2,7 +2,7 @@ from src.data_science_project.utils.common import read_yaml, create_directories
 from src.data_science_project.constants import *
 from src.data_science_project.constants import PARAMS_FILE_PATH
 
-from src.data_science_project.entity.config_entity import DataIngestionConfig
+from src.data_science_project.entity.config_entity import DataIngestionConfig,DataValidationConfig
 
 
 class ConfigurationManager:
@@ -21,4 +21,18 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir)
         return data_ingestion_config
 
-    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
